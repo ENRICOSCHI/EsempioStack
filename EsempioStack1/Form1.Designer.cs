5de76c3df4130ebc39fb3c37b4dffa2f73ccd76a
ENRICOSCHI <fenrico472@gmail.com>
2022-12-15 11:27:29 +0100

    Aggiungere i file di progetto.

---
+++ EsempioStack1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace EsempioStack1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.txtCilindrata = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTarga = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStampa = new System.Windows.Forms.Label();
+            this.btPrint = new System.Windows.Forms.Button();
+            this.txtStampa = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPop);
+            this.groupBox1.Controls.Add(this.btnPush);
+            this.groupBox1.Controls.Add(this.txtCilindrata);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTarga);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(72, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 287);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nuova Automobile";
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(201, 191);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(88, 47);
+            this.btnPop.TabIndex = 5;
+            this.btnPop.Text = "Rimuovi";
+            this.btnPop.UseVisualStyleBackColor = true;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(33, 191);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(88, 47);
+            this.btnPush.TabIndex = 4;
+            this.btnPush.Text = "Aggiungi";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // txtCilindrata
+            // 
+            this.txtCilindrata.Location = new System.Drawing.Point(189, 109);
+            this.txtCilindrata.Name = "txtCilindrata";
+            this.txtCilindrata.Size = new System.Drawing.Size(100, 22);
+            this.txtCilindrata.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cilindrata";
+            // 
+            // txtTarga
+            // 
+            this.txtTarga.Location = new System.Drawing.Point(120, 46);
+            this.txtTarga.Name = "txtTarga";
+            this.txtTarga.Size = new System.Drawing.Size(169, 22);
+            this.txtTarga.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Targa";
+            // 
+            // lblStampa
+            // 
+            this.lblStampa.AutoSize = true;
+            this.lblStampa.Location = new System.Drawing.Point(105, 359);
+            this.lblStampa.Name = "lblStampa";
+            this.lblStampa.Size = new System.Drawing.Size(37, 16);
+            this.lblStampa.TabIndex = 1;
+            this.lblStampa.Text = "label";
+            // 
+            // btPrint
+            // 
+            this.btPrint.Location = new System.Drawing.Point(307, 294);
+            this.btPrint.Name = "btPrint";
+            this.btPrint.Size = new System.Drawing.Size(88, 47);
+            this.btPrint.TabIndex = 6;
+            this.btPrint.Text = "Stampa";
+            this.btPrint.UseVisualStyleBackColor = true;
+            this.btPrint.Click += new System.EventHandler(this.btPrint_Click);
+            // 
+            // txtStampa
+            // 
+            this.txtStampa.Location = new System.Drawing.Point(307, 359);
+            this.txtStampa.Name = "txtStampa";
+            this.txtStampa.Size = new System.Drawing.Size(100, 22);
+            this.txtStampa.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.Controls.Add(this.txtStampa);
+            this.Controls.Add(this.btPrint);
+            this.Controls.Add(this.lblStampa);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.TextBox txtCilindrata;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTarga;
+        private System.Windows.Forms.Label lblStampa;
+        private System.Windows.Forms.Button btPrint;
+        private System.Windows.Forms.TextBox txtStampa;
+    }
+}
+
---
+++ EsempioStack1/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsempioStack1
+{
+    public partial class Form1 : Form
+    {//push==inserire
+     //pop=rimuovo
+        Stack s;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            if(s.push(txtTarga.Text, Int32.Parse(txtCilindrata.Text)))//se ritorna true
+            {
+                MessageBox.Show("Ok");
+            }
+            else
+            {
+                MessageBox.Show("Stack Pieno");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            s = new Stack();
+        }
+
+        private void btPrint_Click(object sender, EventArgs e)
+        {
+            string result = s.pop();
+            if (!string.IsNullOrEmpty(result))
+            {
+                MessageBox.Show("OK,string salvata");
+            }
+            else
+            {
+                MessageBox.Show("Stack vuoto");
+            }
+           // txtStampa.Text = s.toString();
+        }
+    }
+    class Automobile
+    {
+        protected string targa;
+        protected int cilindrata;
+        public Automobile(string t, int c)
+        {
+            targa = t;
+            cilindrata = c;
+        }
+        public string getTarga()
+        {
+            return targa;
+        }
+        public int getCilindrata()
+        {
+            return cilindrata;
+        }
+    }
+    class Stack
+    {
+        protected int first;
+        protected Automobile[] automobili;
+        public Stack()
+        {
+            first = -1;
+            automobili = new Automobile[100];
+        }
+        public bool push(string targa, int cilindrata)
+        {
+            bool result = false;
+            if(first <= automobili.Length)
+            {
+                first++;//da mettere prima, deve aggiungere una parte di memoria 
+                automobili[first] = new Automobile(targa, cilindrata);            
+                result = true;
+            }
+            return result;
+        }
+        // restituisce la targa dell'automobile se presente, altrimenti stringa vuota
+        public string pop()
+        {
+            string item = "";
+            if(first >= 0)
+            {            
+               item = automobili[first].getTarga();//salvo la stringa che sto per togliere
+                automobili[first] = null;
+                first--;//Ritorna indietro l'array(il contenitore)
+
+            }
+            return item;
+        }
+        public string toString()
+        {
+            string result = "";
+            for(int i = 0; i <= first; i++)
+            {
+                result += "targa: " + automobili[i].getTarga();
+                result += " cilindrata: " + automobili[i].getCilindrata() + "\n";
+            }
+            return result;
+        }
+    }
+    class Vettura
+    {
+        protected string targa;
+        protected int cilindrata;
+        protected Vettura next;
+        public  Vettura(string targa,int cilindrata)
+        {
+            this.targa = targa; 
+            this.cilindrata = cilindrata;
+            next = null;
+        }
+        public string getTarga()
+        {
+            return targa;
+        }
+        public int GetCilindrata()
+        {
+            return cilindrata;
+        }
+        public void SetNext(Vettura v)
+        {
+            next = v;
+        }
+        public Vettura getNext()
+        {
+            return next;
+        }
+    }
+    class Pila
+    {
+        protected Vettura first;
+        protected int nnodi;
+        public Pila()
+        {
+            nnodi = 0;
+            first = null;
+        }
+        public void push(Vettura v)
+        {
+            if(nnodi == 0)
+            {
+                first = v;
+                nnodi++;
+            }
+            else
+            {
+                v.SetNext(first);
+                first = v.getNext();
+                nnodi++;
+            }
+        }
+        public Vettura pop()
+        {
+            Vettura v= null;
+            if (nnodi != 0)
+            {
+                v = first;
+                first = v.getNext();
+                nnodi--;
+            }
+            return v;
+        }
+        public string TOString()
+        {
+            string s = "";
+            Vettura v = first;
+            if (v != null)
+            {
+                while (v != null)
+                {
+                    s += v.getTarga();
+                    s += v.GetCilindrata();
+                    v = v.getNext();
+                }
+            }
+            return s;
+        }
+    }
+}
